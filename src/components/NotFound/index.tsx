0bbc64984f426c915a4a2a24b8b00248f2d87481
falkow <falkowskigrz@gmail.com>
2023-03-22 21:31:59 +0100

    change NotFound page/component change button/link styling

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,3 +1,35 @@
-export const NotFound = (/* { ...product }: DummyCard */) => {
-  return <div>NOT FOUND!!!!!!!!!!</div>;
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import scarecrow from '../../assets/Scarecrow.png';
+import styles from './rwd.module.scss';
+
+const {
+  wrapper,
+  wrapperHeader,
+  wrapperMain,
+  wrapperMainScarecrow,
+  wrapperMainInform,
+} = styles;
+
+export const NotFound = () => {
+  return (
+    <div className={wrapper}>
+      <div className={wrapperHeader}>
+        <h1>404 not found</h1>
+      </div>
+      <section className={wrapperMain}>
+        <div className={wrapperMainScarecrow}>
+          <img src={scarecrow} alt='Scarecrow' />
+        </div>
+        <div className={wrapperMainInform}>
+          <h2>I have bad news for you</h2>
+          <span>
+            The page you are looking for might be removed or is temporarily
+            unavailable
+          </span>
+          <Link to='/'>back to homepage</Link>
+        </div>
+      </section>
+    </div>
+  );
 };
