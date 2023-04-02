ba99f6bb23b83463800e9331b35f9e262892297c
falkow <falkowskigrz@gmail.com>
2023-04-02 20:07:38 +0200

    created new file to import/export pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CartProvider from './context/Cart/CartProvider';
-import { NotFound } from './components/NotFound';
 import ProductProvider from './context/Product/ProductProvider';
-import './styles/global.scss';
 import ScrollToTop from './utils/ScrollToTop';
-import CartPage from './pages/CartPage';
-import ShopPage from './pages/ShopPage';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
+import { CartPage, HomePage, NotFound, ProductPage, ShopPage } from './pages';
+
+import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ProductProvider>
@@ -18,12 +15,12 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
-          <Route path='/cart' element={<CartPage />} />
           <Route
             path='/shop/:id'
             element={<ProductPage />}
             errorElement={<NotFound />}
           />
+          <Route path='/cart' element={<CartPage />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import CartPage from './CartPage';
+import HomePage from './HomePage';
+import ProductPage from './ProductPage';
+import ShopPage from './ShopPage';
+import { NotFound } from '../components/NotFound';
+
+export { CartPage, HomePage, ProductPage, ShopPage, NotFound };
